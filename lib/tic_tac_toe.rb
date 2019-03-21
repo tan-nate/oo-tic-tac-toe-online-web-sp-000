f8f55f93aec057bf86bea13bd147b6cbc676de15
Nate Tan <nktan93@gmail.com>
2019-03-21 19:27:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,6 +39,12 @@ class TicTacToe
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end
+  
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+  
+  
 end
 
 #binding.pry
