29feb994f72411d815b8edc37a5d25509780b973
Nate Tan <nktan93@gmail.com>
2019-03-21 19:13:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,7 +26,7 @@ class TicTacToe
     user_input.to_i - 1
   end
   
-  def move(board, index, current_player)
-    board[index] = current_player
+  def move(index, current_player)
+    @board[index] = current_player
   end
 end
