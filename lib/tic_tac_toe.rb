c3b65c5e804ae138971786012d22ef134f49aa4e
Nate Tan <nktan93@gmail.com>
2019-03-21 19:22:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,7 +33,7 @@ class TicTacToe
   end
   
   def position_taken?(board, location)
-    board[location] != " " && board[location] != ""
+    @board[location] != " " && @board[location] != ""
   end
 end
 
