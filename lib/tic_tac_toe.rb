78df21429a4fd82a2c81c9a66cb5ba9cb9d8000e
Nate Tan <nktan93@gmail.com>
2019-03-21 19:35:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,9 +95,9 @@ class TicTacToe
     end
   end
   
-  def winner(board)
-    if won?(board)
-      board[won?(board)[0]]
+  def winner
+    if won?
+      @board[won?(@board)[0]]
     else
       nil
     end
